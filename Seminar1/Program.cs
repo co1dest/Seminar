0e0bf687571474d0008a27ba7d4632159e26c4e0
Boronin Alexander <co1dest@yandex.ru>
2022-12-06 16:12:54 +0300

    new fail

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -2,23 +2,55 @@
 
  // Задача №1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
- Console.Write("Input ferst number: ");
+/*
+Console.Write("Input ferst number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+ 
+int max = num1;
+ 
+if (max > num2)
+{
+   Console.Write(max);
+}
+else
+{
+   max = num2;
+   Console.Write(max);
+}
+*/
+ 
+ // Задача №2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+
+ Console.Write("Input fest number: ");
  int num1 = Convert.ToInt32(Console.ReadLine());
  Console.Write("Input second number: ");
  int num2 = Convert.ToInt32(Console.ReadLine());
- 
+ Console.Write("Input third number: ");
+ int num3 = Convert.ToInt32(Console.ReadLine());
+
  int max = num1;
- 
- if (max > num2)
- {
-    Console.Write(max);
- }
- else
- {
-max = num2;
+
+if(max > num2)
+{
+
+}
+else
+{
+    max = num2;
+}
+if(max > num3)
+{
+    
+}
+else
+{
+    max = num3;
+}
 Console.Write(max);
- }
 
- // Задача №2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+*/
 
- 
+// Задача №3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
