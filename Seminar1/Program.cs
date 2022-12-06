0bd8fa5d5cd550675518c2008a57388ce5d9cd22
Boronin Alexander <co1dest@yandex.ru>
2022-12-06 15:17:57 +0300

    Zadacha 1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,24 @@
+﻿ // Семинар 1. Знакомство
+
+ // Задача №1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+ Console.Write("Input ferst number: ");
+ int num1 = Convert.ToInt32(Console.ReadLine());
+ Console.Write("Input second number: ");
+ int num2 = Convert.ToInt32(Console.ReadLine());
+ 
+ int max = num1;
+ 
+ if (max > num2)
+ {
+    Console.Write(max);
+ }
+ else
+ {
+max = num2;
+Console.Write(max);
+ }
+
+ // Задача №2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+ 
