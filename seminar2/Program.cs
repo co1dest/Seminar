6418d28a08889f0edbd7cf8d7fcfeb3354789942
Boronin Alexander <co1dest@yandex.ru>
2022-12-08 10:04:51 +0300

    b

--- seminar2/Program.cs
+++ seminar2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int CutNumber(int num)
+{
+    int sot = num / 100;
+    int ed = num % 10;
+
+    int result = sot * 10 + ed;
+    return result;
+}
+
+int randNum = new Random().Next(100, 1000);
+int newNum = CutNumber(randNum);
+
+Console.WriteLine($"New version of a number {randNum} is {newNum}");
